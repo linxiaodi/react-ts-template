de5b4e7d8cd6b81d675503c7265448b1b592081d
doudou <1443970739@qq.com>
2020-06-29 14:53:44 +0800

    feat:删除dashboard、新增首页

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -1,15 +1,10 @@
-import styles from './home.module.less'
-import logo from '@assets/images/logo.svg'
 import React from 'react'
-import { renderAllRoutes } from '@routes/route-loader'
-import { Switch, RouteComponentProps } from 'react-router-dom'
+import { RouteComponentProps } from 'react-router-dom'
 
 function mapStateToProps(state) {
     return state
 }
-/**
- * 路由参数 Props 类型声明
- */
+
 interface RouterProps extends RouteComponentProps<any> {}
 
 function Home(props) {
